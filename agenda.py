2c999bf59603b622e6772923b7f7c389a075fad0
rahmansaleh7 <rahman.saleh7@gmail.com>
2019-12-16 16:34:10 +0700

    Memperbaiki agenda

--- agenda.py
+++ agenda.py
@@ -2,7 +2,9 @@ from odoo import api, fields, models, _
 
 class Agenda(models.Model):
 	_name = "manajemen.agenda"
-	_rec_name = "name"
 
-	name = fields.Char("Name", required=True)
-	no = fields.Text(string="No", required=False, )
+	no = fields.Char(string="No", required=True)
+	tema = fields.Text(string="Tema", required=False, )
+	tglmulai = fields.Text(string="Tanggal Mulai", required=False, )
+	tglselesai = fields.Text(string="Tanggal Selesai", required=False, )
+	aksi = fields.Text(string="Aksi", required=False, )
