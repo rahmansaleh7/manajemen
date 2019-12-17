a665d902c5483dced2570d5fd1931e4057ef2342
rahmansaleh7 <rahman.saleh7@gmail.com>
2019-12-17 16:58:29 +0700

    mengedit sub ruang

--- subruang.py
+++ subruang.py
@@ -5,6 +5,5 @@ class Subruang(models.Model):
 
 	no = fields.Char(string="No", required=True)
 	ruang = fields.Many2one(comodel_name="manajemen.ruang", string="Ruang", required=True, )
-	nosubruang = fields.Text(string="No Sub Ruang", required=False, )
+	nosubruang = fields.Char(string="No Sub Ruang", required=False, )
 	name = fields.Char(string="Nama Sub Ruang", required=True, )
-	aksi = fields.Text(string="Aksi", required=False, )
